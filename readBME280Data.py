e5a11c9aa44c9e1e7eae9a7e0d51d17a027304e9
PilarD44 <91191799+PilarD44@users.noreply.github.com>
2022-05-17 16:50:28 +0200

    Update readBME280Data.py

--- readBME280Data.py
+++ readBME280Data.py
@@ -1,22 +1,25 @@
 import serial
 
-cansatConn = serial.Serial("com6", "9600")
+cansatConn = serial.Serial("com4", "9600")
 
 
 
 while True:
     if (cansatConn.inWaiting()>0):
         data = str(cansatConn.readline())
-        print(str(data))
-
+        print(data)
         #slicing strings
         aposPos = data.find("'")
         comaPos = data.find(",")
-        colonPos = data.find(":")
-        semiColonPos = data.find(";")
-        hyphenPos = data.find("-")
+        comaPos2 = data.find(",",comaPos + 1)
+        comaPos3 = data.find(",", comaPos2 + 1)
+        dashPos = data.find("\\")
 
         temperature = data[aposPos + 1: comaPos]
-        pressure = data[comaPos + 2: colonPos]
-        altitude = data[colonPos + 2: semiColonPos]
-        humidity = data[semiColonPos + 2: hyphenPos]
+        pressure = data[comaPos + 2: comaPos2]
+        altitude = data[comaPos2 + 2: comaPos3]
+        humidity = data[comaPos3 + 2: dashPos]
+
+        # printing values (variables)
+        print(f'|temperature = {temperature}| pressure = {pressure}| altitude = {altitude}| humidity = {humidity}|')
+
