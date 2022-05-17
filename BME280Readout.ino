7e0701a0d1e09458d5d2b71f6ded52df390505d9
PilarD44 <91191799+PilarD44@users.noreply.github.com>
2022-05-17 16:49:44 +0200

    Update BME280Readout.ino

--- BME280Readout.ino
+++ BME280Readout.ino
@@ -1,16 +1,12 @@
 /***************************************************************************
   This is a library for the BME280 humidity, temperature & pressure sensor
-
   Designed specifically to work with the Adafruit BME280 Breakout
   ----> http://www.adafruit.com/products/2650
-
   These sensors use I2C or SPI to communicate, 2 or 4 pins are required
   to interface. The device's I2C address is either 0x76 or 0x77.
-
   Adafruit invests time and resources providing this open source code,
   please support Adafruit andopen-source hardware by purchasing products
   from Adafruit!
-
   Written by Limor Fried & Kevin Townsend for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
   See the LICENSE file for details.
@@ -61,13 +57,6 @@ void setup() {
     Serial.println();
 }
 
-
-void loop() { 
-    printValues();
-    delay(delayTime);
-}
-
-
 void printValues() {
     
     Serial.print(bme.readTemperature());
@@ -75,14 +64,20 @@ void printValues() {
     
 
     Serial.print(bme.readPressure() / 100.0F);
-    Serial.print(": ");
+    Serial.print(", ");
     
     Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.print("; ");
+    Serial.print(", ");
 
     Serial.print(bme.readHumidity());
-    Serial.print("-");
+    
  
 
     Serial.println();
 }
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
