8b26d2142320025cc8dd3b88e3c4b34c40877d8a
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2022-05-31 13:44:11 +0200

    test graphs with random variables

--- readBME280Data.py
+++ readBME280Data.py
@@ -1,25 +1,52 @@
 import serial
+import matplotlib.pyplot as plt
+import numpy as np
+import time as tm
+import random
 
-cansatConn = serial.Serial("com4", "9600")
+T = []
+P = []
+A = []
+H = []
+lists = [T, P, A, H]
 
+fig = plt.figure()
+t_ax = fig.add_subplot(221, title="Temperature graph")
+p_ax = fig.add_subplot(222, title="Pressure graph")
+a_ax = fig.add_subplot(223, title="Altitude graph")
+h_ax = fig.add_subplot(224, title="Humidity graph")
+plt.tight_layout()
 
+# cansatConn = serial.Serial("com4", "9600")
 
 while True:
-    if (cansatConn.inWaiting()>0):
-        data = str(cansatConn.readline())
-        print(data)
-        #slicing strings
-        aposPos = data.find("'")
-        comaPos = data.find(",")
-        comaPos2 = data.find(",",comaPos + 1)
-        comaPos3 = data.find(",", comaPos2 + 1)
-        dashPos = data.find("\\")
+    # if (cansatConn.inWaiting()>0):
+    #     data = str(cansatConn.readline())
+    #     print(data)
+    #     #slicing strings
+    #     aposPos = data.find("'")
+    #     comaPos = data.find(",")
+    #     comaPos2 = data.find(",",comaPos + 1)
+    #     comaPos3 = data.find(",", comaPos2 + 1)
+    #     dashPos = data.find("\\")
 
-        temperature = data[aposPos + 1: comaPos]
-        pressure = data[comaPos + 2: comaPos2]
-        altitude = data[comaPos2 + 2: comaPos3]
-        humidity = data[comaPos3 + 2: dashPos]
+    #     temperature = data[aposPos + 1: comaPos]
+    #     pressure = data[comaPos + 2: comaPos2]
+    #     altitude = data[comaPos2 + 2: comaPos3]
+    #     humidity = data[comaPos3 + 2: dashPos]
 
         # printing values (variables)
-        print(f'|temperature = {temperature}| pressure = {pressure}| altitude = {altitude}| humidity = {humidity}|')
+        tm.sleep(1)
 
+        T.append(random.uniform(0,10))
+        P.append(random.uniform(0,10))
+        A.append(random.uniform(0,10))
+        H.append(random.uniform(0,10))
+        
+        t_ax.plot(np.arange(len(T)), T, color="blue")
+        p_ax.plot(np.arange(len(P)), P, color="blue")
+        a_ax.plot(np.arange(len(A)), A, color="blue")
+        h_ax.plot(np.arange(len(H)), H, color="blue")
+        
+        plt.pause(0.001)
+        
