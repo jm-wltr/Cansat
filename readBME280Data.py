e10acc786194b6634c6c11c9cc29be4417565662
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2022-10-21 12:33:01 +0200

    added database functionality which WORKS

--- readBME280Data.py
+++ readBME280Data.py
@@ -1,11 +1,13 @@
 import serial
 import matplotlib.pyplot as plt
 import numpy as np
+import mariadb
+import sys
 
 T = []
 P = []
-A = []
 H = []
+A = []
 
 fig = plt.figure()
 t_ax = fig.add_subplot(221, title="Temperature graph", ylabel = "θ/ºC")
@@ -16,30 +18,52 @@ plt.tight_layout()
 
 cansatConn = serial.Serial("com8", "9600")
 
-while True:
-    if (cansatConn.inWaiting()>0):
-        data = str(cansatConn.readline())
-        print(data)
-        #slicing strings
-        aposPos = data.find("'")
-        comaPos = data.find(",")
-        comaPos2 = data.find(",",comaPos + 1)
-        comaPos3 = data.find(",", comaPos2 + 1)
-        dashPos = data.find("\\")
+try:
+    conn = mariadb.connect(
+        user = "root",
+        password="Alt2020!",
+        host="127.0.0.1",
+        port = 3306,
+        database="cansattest1"
+        )
 
-        temperature = data[aposPos + 1: comaPos]
-        pressure = data[comaPos + 2: comaPos2]
-        altitude = data[comaPos2 + 2: comaPos3]
-        humidity = data[comaPos3 + 2: dashPos]
+except mariadb.Error as e:
+    print(f"Error onnecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
-        T.append(float(temperature))
-        P.append(float(pressure))
-        A.append(float(altitude))
-        H.append(float(humidity))
-        
-        t_ax.plot(np.arange(len(T)), T, color="blue")
-        p_ax.plot(np.arange(len(P)), P, color="blue")
-        a_ax.plot(np.arange(len(A)), A, color="blue")
-        h_ax.plot(np.arange(len(H)), H, color="blue")
-        
-        plt.pause(0.001)
+with conn.cursor() as cur:
+
+    while True:
+        if (cansatConn.inWaiting()>0):
+            data = str(cansatConn.readline())
+            print(data)
+            #slicing strings
+            aposPos = data.find("'")
+            comaPos = data.find(",")
+            comaPos2 = data.find(",",comaPos + 1)
+            comaPos3 = data.find(",", comaPos2 + 1)
+            dashPos = data.find("\\")
+
+            temperature = data[aposPos + 1: comaPos]
+            pressure = data[comaPos + 2: comaPos2]
+            altitude = data[comaPos2 + 2: comaPos3]
+            humidity = data[comaPos3 + 2: dashPos]
+
+            cur.execute(
+            "INSERT INTO datatable1 (temperature, pressure, altitude, humidity) VALUES (?, ?, ?, ?);",
+            (temperature, pressure, altitude, humidity))
+
+            conn.commit() 
+
+            T.append(float(temperature))
+            P.append(float(pressure))
+            A.append(float(altitude))
+            H.append(float(humidity))
+
+            t_ax.plot(np.arange(len(T)), T, color="blue")
+            p_ax.plot(np.arange(len(P)), P, color="blue")
+            a_ax.plot(np.arange(len(A)), A, color="blue")
+            h_ax.plot(np.arange(len(H)), H, color="blue")
+
+            plt.pause(0.001)
+
